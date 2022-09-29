e697b76e0992ab5ac361920152edea6d8ac939b9
unknown <Hiru1728>
2022-09-29 16:34:44 +0600

    show Break time

--- src/Component/Information/Information.js
+++ src/Component/Information/Information.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import image from '../../images/7083.jpg'
 import StudyDetails from '../../StudyDetails/StudyDetails';
 import './Information.css'
 
 const Information = ({ informations }) => {
+    const [firstButton, setFirstButton] = useState([]);
+
+    const handleBreakBtn = (breakTime) => {
+        console.log(breakTime);
+        setFirstButton(breakTime);
+    }
     return (
         <div className='information'>
             <div className='img-name'>
@@ -17,19 +23,20 @@ const Information = ({ informations }) => {
             </div>
             <h4>Added a Break</h4>
             <div className='btn-section'>
-                <button className='btn-text'>10s</button>
-                <button className='btn-text'>40s</button>
-                <button className='btn-text'>30s</button>
-                <button className='btn-text'>20</button>
+                <button onClick={() => handleBreakBtn(10)} className='btn-text'>10s</button>
+                <button onClick={() => handleBreakBtn(40)} className='btn-text'>40s</button>
+                <button onClick={() => handleBreakBtn(30)} className='btn-text'>30s</button>
+                <button onClick={() => handleBreakBtn(20)} className='btn-text'>20</button>
             </div>
             <h4>Study Details</h4>
             <StudyDetails
                 informations={informations}
+                firstButton={firstButton}
             ></StudyDetails>
             <button className='btn-active'>
                 <h3>Activity Completed</h3>
             </button>
-        </div>
+        </div >
     );
 };
 
--- src/StudyDetails/StudyDetails.js
+++ src/StudyDetails/StudyDetails.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
-const StudyDetails = ({ informations }) => {
-
+const StudyDetails = ({ informations, firstButton }) => {
+    console.log(firstButton);
     let totalTime = 0;
     for (const subject of informations) {
         totalTime = totalTime + parseInt(subject.time);
     }
-    console.log(totalTime);
     return (
         <div>
             <div className='study-time'>
                 <p>Study Time: {totalTime} hours</p>
             </div>
             <div className='break-time'>
-                <p>Break Time: 0 second </p>
+                <p>Break Time: {firstButton} second </p>
             </div>
         </div>
     );
